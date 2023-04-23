f3ebcccdef4e245ac48413acd6628e2dd7f4744d
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-04-23 21:59:49 +0900

    Add @dealer to week1-2 01_BLACKJACK game.rb

--- week1-2/03_submission_quest/01_BLACKJACK/game.rb
+++ week1-2/03_submission_quest/01_BLACKJACK/game.rb
@@ -5,11 +5,14 @@ class Game
   def initialize
     @deck = Deck.new
     @player = Player.new("あなた")
+    @dealer = Player.new("ディーラー")
   end
   
   def start
     puts "ブラックジャックを開始します"
     puts "#{@player.name}の引いたカードは#{@deck.draw}です"
     puts "#{@player.name}の引いたカードは#{@deck.draw}です"
+    puts "#{@dealer.name}の引いたカードは#{@deck.draw}です"
+    puts "#{@dealer.name}の引いたカードは#{@deck.draw}です"
   end
 end
