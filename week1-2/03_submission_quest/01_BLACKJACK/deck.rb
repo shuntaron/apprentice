9edfa9859302a7ee527702f242ea28970d53a9f5
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-04-23 21:21:25 +0900

    Add week1-2 01_BLACKJACK deck.rb

---
+++ week1-2/03_submission_quest/01_BLACKJACK/deck.rb
@@ -0,0 +1,15 @@
+class Deck
+  attr_reader :deck
+  
+  def initialize
+    @deck = []
+    suits = %w(ハート スペード クラブ ダイヤ)
+    numbers = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+    
+    suits.each do |suit|
+      numbers.each do |number|
+        @deck << [suit, number]
+      end
+    end
+  end
+end
--- week1-2/03_submission_quest/01_BLACKJACK/game.rb
+++ week1-2/03_submission_quest/01_BLACKJACK/game.rb
@@ -1,5 +1,14 @@
+require_relative "deck"
+
 class Game
+  def initialize
+    @deck = Deck.new
+  end
+  
   def start
     puts "ブラックジャックを開始します"
+    @deck.deck.each do |card|
+      p card
+    end
   end
 end
