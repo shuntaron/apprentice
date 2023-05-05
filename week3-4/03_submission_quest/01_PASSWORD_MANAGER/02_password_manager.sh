4d57cb0ead6b816c4c8a7053dff08db8f02ae936
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-05-06 07:25:05 +0900

    Add comment week3-4 01_PASSWORD_MANAGER 02_password_manager.sh

--- week3-4/03_submission_quest/01_PASSWORD_MANAGER/02_password_manager.sh
+++ week3-4/03_submission_quest/01_PASSWORD_MANAGER/02_password_manager.sh
@@ -3,11 +3,14 @@
 echo "パスワードマネージャーへようこそ！"
 echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
 
+# Exit を入力するまで繰り返し
 while :
 do
   read option
   
   case $option in
+  
+    # Add Password が入力された場合
     "Add Password" )
       echo "サービス名を入力してください："
       read service
@@ -17,47 +20,62 @@ do
       read -s password
       echo "$service:$username:$password" >> 02_password.txt
       echo "パスワードの追加は成功しました。"
+      
       echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
       ;;
-      
+    
+    # Get Password が入力された場合
     "Get Password" )
       echo "サービス名を入力してください："
       read service
       
+      # パスワード管理ファイルのサービス名を部分一致でgrepし、マッチした行を変数へ格納
       result=$(grep -E "^.*$service.*:.*:.*" 02_password.txt)
       
+      # サービス名が保存されていなかった場合
       if [[ -z $result ]]; then
-        ## サービス名が保存されていなかった場合
         echo "そのサービスは登録されていません。"
+      # サービス名が保存されていた場合
       else
-        ## サービス名が保存されていた場合
-        
+        # デフォルトの区切り文字($' \t\n')を変数へバックアップ
         OLDIFS=$IFS
-        IFS='\n'
+        # デフォルトの区切り文字($' \t\n')を削除
+        #  (後続のechoで改行を区切り文字として処理させないため)
+        unset $IFS
         
+        # grepでマッチした行数分繰り返し
         echo $result | while read line
         do
-          service=$(echo $line | cut -f1 -d:)
-          username=$(echo $line | cut -f2 -d:)
-          password=$(echo $line | cut -f3 -d:)
+          # サービス名:ユーザー名:パスワードの各項目を変数へ格納
+          service=$(echo $line  | cut -d: -f1)
+          username=$(echo $line | cut -d: -f2)
+          password=$(echo $line | cut -d: -f3)
+          
+          # 結果を出力
           echo "サーピス名:$service"
           echo "ユーザー名:$username"
           echo "パスワード:$password"
           echo ""
         done
         
+        # 区切り文字をデフォルト値へ戻す
         IFS=$OLDIFS
       fi
+      
       echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
       ;;
+    
+    # Exit が入力された場合
     "Exit" )
       break
       ;;
-      
+    
+    # Add Password/Get Password/Exit 以外が入力された場合
     * )
       echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
       ;;
   esac
 done
 
+# プログラム終了
 echo "Thank you!"
