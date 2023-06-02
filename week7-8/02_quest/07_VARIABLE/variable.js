4b772a2ef4d37384d9eecb41a0dc258a5ed56490
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-06-02 21:36:22 +0900

    Add week7-8 07_VARIABLE

---
+++ week7-8/02_quest/07_VARIABLE/variable.js
@@ -0,0 +1,14 @@
+// 再代入できない変数 num_1 と num_2 を定義
+const num_1 = 5
+const num_2 = 3
+
+// 再代入できる変数 sum を定義
+let sum = num_1 + num_2
+console.log(sum)
+
+// 再代入できる変数 difference を定義
+let difference  = num_1 - num_2
+console.log(difference)
+
+// sum と difference を足した値をコンソールに出力
+console.log(sum + difference)
