aa0a23f044b0ea7697963b02a8fa0b9c0574df1d
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-06-20 22:10:50 +0900

    Add week9-10 08_USE_API action

--- week9-10/02_quest/08_USE_API/use-api/app/controllers/todos_controller.rb
+++ week9-10/02_quest/08_USE_API/use-api/app/controllers/todos_controller.rb
@@ -1,2 +1,37 @@
 class TodosController < ApplicationController
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: { todo: todo }, except: [:created_at, :updated_at]
+    else
+      render json: { error: todo.errors.full_messages }
+    end
+  end
+  
+  def index
+    todos = Todo.all
+    render json: { todos: todos }, except: [:created_at, :updated_at]
+  end
+  
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update(todo_params)
+      render json: { todo: todo }, except: [:created_at, :updated_at]
+    else
+      render json: { error: todo.errors.full_messages }
+    end
+  end
+  
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    render status: :no_content
+  end
+  
+  private
+  
+  def todo_params
+    params.require(:todo).permit(:title)
+  end
+  
 end
