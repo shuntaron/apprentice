2b02058fe8908956cd67196a260d7056d36b5759
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-06-23 23:11:59 +0900

    Add week9-10 14_UNIT_TEST create test

--- week9-10/02_quest/14_UNIT_TEST/unit-test-api/test/models/todo_test.rb
+++ week9-10/02_quest/14_UNIT_TEST/unit-test-api/test/models/todo_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # TODO タスクのタイトルが空だった場合、バリデーションエラーになる
+  test "empty title should raise validation error" do
+    todo = Todo.new(title: "")
+    assert_not todo.valid?
+    assert_includes todo.errors[:title], "can't be blank"
+  end
+  
+  # TODO タスクのタイトルが入力されていた場合、バリデーションエラーにならない
+  test "title should not raise validation error" do
+    todo = Todo.new(title: "test")
+    assert todo.valid?
+  end
 end
